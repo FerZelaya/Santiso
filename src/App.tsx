aecb595cf7b99b95fbc1b3f8cb9c6bf47ddea372
FerZelaya <fernandozelaya64@hotmail.com>
2026-05-19 22:56:45 -0600

    Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { About } from './components/About';
-import { Benefits } from './components/Benefits';
-import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
-import { Hero } from './components/Hero';
-import { MermeladasSection } from './components/MermeladasSection';
-import { Navbar } from './components/Navbar';
-import { ProductCategories } from './components/ProductCategories';
-import { TomateSection } from './components/TomateSection';
-import { ValueBadges } from './components/ValueBadges';
-import { VegetalesSection } from './components/VegetalesSection';
-import { Wholesale } from './components/Wholesale';
+import { About } from "./components/About";
+import { Benefits } from "./components/Benefits";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { MermeladasSection } from "./components/MermeladasSection";
+import { Navbar } from "./components/Navbar";
+import { ProductCategories } from "./components/ProductCategories";
+import { TomateSection } from "./components/TomateSection";
+import { ValueBadges } from "./components/ValueBadges";
+import { VegetalesSection } from "./components/VegetalesSection";
+import { Wholesale } from "./components/Wholesale";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
         <Contact />
       </main>
       <Footer />
+      <Analytics />
     </>
   );
 }
